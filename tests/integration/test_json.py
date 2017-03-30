8c60957af6d7682dce297842eca1d9c8a4713e3f
kdaily <kenneth.daily@sagebase.org>
2017-03-30 09:23:26 -0700

    pep8 something something

--- tests/integration/test_json.py
+++ tests/integration/test_json.py
@@ -6,12 +6,14 @@ import os
 import glob
 import json
 
+
 def setup_module(module):
     # Get a list of JSON files
     test_file = os.path.abspath(__file__)
     test_dir = os.path.split(test_file)[0]
     module.json_files = map(lambda x: os.path.abspath(x),
                             glob.glob("%s/../../*.json" % test_dir))
+
 
 def test_duplicate_values_within_key():
     for json_file in json_files:
@@ -20,4 +22,7 @@ def test_duplicate_values_within_key():
             for k, v in d.iteritems():
                 value_count = set([x for x in v if v.count(x) > 1])
                 if value_count:
-                    raise ValueError("Duplicate values for key '{key}': {values} in {file}" .format(key=k, values=list(value_count), file=json_file))
+                    raise ValueError(
+                        "Duplicate values for key '%s': %s in %s" % (k,
+                                                                     list(value_count),
+                                                                     json_file))
