c49420c9f7fe1126c6daa1c5281fd2000998444b
teslajoy <nasim@plenary.org>
2017-06-21 16:27:29 -0700

    updating table rows by 3 row-change cases

--- scripts/json2synapse.py
+++ scripts/json2synapse.py
@@ -1,10 +1,15 @@
+import sys
 import pandas
+import numpy
 import synapseclient
+from synapseclient import Table
 from pandas.io.json import json_normalize
+import logging
 
 syn = synapseclient.login()
+logger = logging.getLogger(__name__)
 
-# TODO: replace with auto download of all github raw files in repositority 
+# TODO: replace with auto download of raw files in repo and replace print with logger 
 standard_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/common/minimal_Sage_standard.json'
 analysis_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/common/minimal_Sage_analysis.json'
 dhart_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/dhart_annotations.json'
@@ -13,26 +18,28 @@ neuro_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotati
 nf_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/nf_annotations.json'
 ngs_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/ngs_annotations.json'
 onc_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/onc_annotations.json'
+# tool1_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/tool_annotations.json'
+# tool2_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/tool_annotations_extended.json'
 
-# global variables: paths to raw json files and their project name
 paths = [standard_path, analysis_path, dhart_path, genie_path, neuro_path, nf_path, ngs_path, onc_path]
 names = ['standard', 'analysis', 'dhart', 'genie', 'neuro', 'nf', 'ngs', 'onc']
-
-# for each project save normalized json to a list
 all_projects = []
+
+tableSynId = "syn10005388"
+key = ["key", "value"]
 
 
 def json2flatten(path, project):
     # fetch and read raw json objects from its github url and decode the json object to its raw format
     json_record = pandas.read_json(path)
 
-    # for each value list object save normalized df to a list
+    # for each value list object
     flatten_vals = []
 
     for i, jsn in enumerate(json_record['enumValues']):
         if not json_record['enumValues'][i]:
             # replace NAN / NA with enumvalue objects with an uniform schema holding empty strings
-            rows = json_record.ix[i:i, json_record.columns != 'enumValues']
+            rows = json_record.loc[i:i, json_record.columns != 'enumValues']
             rows["values_description"] = ""
             rows["source"] = ""
             rows["value"] = ""
@@ -43,9 +50,8 @@ def json2flatten(path, project):
             df = df.rename(columns={'value': 'enumValues_value', 'description': 'enumValues_description',
                                     'source': 'enumValues_source'})
 
-            # grab key information in its row, expand it by values dimention and append its key-columns to flattened
-            # values
-            rows = json_record.ix[i:i, json_record.columns != 'enumValues']
+            # grab key information in its row, expand it by values dimention and append its key-columns to flattened values
+            rows = json_record.loc[i:i, json_record.columns != 'enumValues']
             repeats = pandas.concat([rows] * len(df.index))
             repeats.set_index(df.index, inplace=True)
             flatten_df = pandas.concat([repeats, df], axis=1)
@@ -59,34 +65,144 @@ def json2flatten(path, project):
     return flatten_vals
 
 
-for i, p in enumerate(paths):
-    data = json2flatten(p, names[i])
-    project_df = pandas.concat(data)
-    all_projects.append(project_df)
-
-# append/concat/collapse all normalized dataframe annotations into one dataframe
-all_projects_df = pandas.concat(all_projects)
-
-# re-arrange columns/fields 
-all_projects_df = all_projects_df[
-    ["name", "description", "columnType", "maximumSize", "enumValues_value", "enumValues_description",
-     "enumValues_source", "project"]]
-
-# html sources requires to be in utf-8 encode format
-all_projects_df.enumValues_description = all_projects_df.enumValues_description.str.encode("utf-8")
-all_projects_df.enumValues_source = all_projects_df.enumValues_source.str.encode("utf-8")
-all_projects_df.enumValues_value = all_projects_df.enumValues_value.str.encode("utf-8")
-
-# TODO:  update table without appending existing rows in table (Tom | Bruce)
-# Temporary solution: save file to a csv file
-all_projects_df.to_csv("annot.csv", sep=',', index=False, encoding="utf-8")
+def rightJoin(df1, df2, key, suffixes):
+    # rows in df2 that are not in df1 based on unique keys (can be a list) of both dataframes
+    df3 = pandas.merge(df1, df2, on=key, how='right', indicator='Exist', suffixes=suffixes)
+    df3['Exist'] = numpy.where(df3.Exist == 'both', True, False)
+    return df3
 
 
+def outerJoin(df1, df2):
+    # rows in df1 that are not in df2 
+    df3 = pandas.merge(df1, df2, how='outer', indicator=True).query('_merge == "left_only"').drop(['_merge'], axis=1)
+    return df3
 
 
+def updateTable(tableSynId, newTable, key, delta=False, whereClause=False):
+    """
+    A table may have one-many key(s) in a table schema predefined by the user
+    There are three cases where changes can be done to a table:
+
+        Case 1: The unique keys have been changed but the remaining schema is the same
+        Case 2: The schema has been changed but the keys are the same
+        Case 3: New rows that have been added to table
+
+    :param tableSynId: the synapse table Id
+    :param newTable: synapseAnnotations normalized and melted json files into a new dataframe
+    :param key: the unique identifiers of the schema in our case the tuple (key-value)
+    :param delta: if previous version of changes should not be deleted
+    :param whereClause: synapse table query where clause that follows a SQL language syntax
+    :return: None and table on synapse identified by tableSynId should be updated
+    """
+
+    currentTable = syn.tableQuery("SELECT * FROM %s" % tableSynId)
+    currentTable = currentTable.asDataFrame()
+
+    if sum(pandas.Series(key).isin(currentTable.columns)) == 0:
+        sys.exit("All unique keys in schema don't exist")
+    elif sum(pandas.Series(key).isin(currentTable.columns)) == 1:
+        sys.exit("One of unique keys in schema don't exist")
+    else:
+        print("Unique keys in schema exist and pursuing")
+
+    # Test cases: change in a non-key column cell and change in value one of unique keys
+    # newTable.iloc[3, newTable.columns.get_loc('valuesDescription')] = 'exome sequencing'
+    # newTable.iloc[1, newTable.columns.get_loc('value')] = 'miRNASEQ'
+
+    # standardize na/NAN in data by replacing it with an empty string for match comparisons
+    currentTable = currentTable.fillna("")
+    newTable = newTable.fillna("")
+
+    # Match schema in current and new tables
+    schema = currentTable.columns
+    newTable = newTable[currentTable.columns]
+
+    # Keep the ROW_ID of the current Table
+    currentTable['index'] = currentTable.index
+
+    ## Case 1: find the rows where the unique keys have been changed but the remaining schema is the same
+    resultDF = rightJoin(df1=currentTable, df2=newTable, key=key, suffixes=('_cur', '_new'))
+
+    newCol = key + [s + '_new' for s in [col for col in newTable.columns if col not in key]]
+    changedRows = resultDF.loc[resultDF.Exist == False, newCol].drop_duplicates()
+
+    newCol = [c.replace('_new', '') for c in newCol]
+    changedRows.columns = newCol
+
+    # column names in schema and not in keys
+    notKey = [col for col in newTable.columns if col not in key]
+    # standardize na/NAN in data by replacing it with an empty string for match comparisons
+    changedRows = changedRows.fillna("")
+
+    changedKeys = rightJoin(df1=changedRows, df2=currentTable, key=notKey, suffixes=('_new', '_cur'))
+
+    changedKeys.columns = [c.replace('_cur', '') for c in changedKeys.columns]
+    changedKeysRows = changedKeys.loc[changedKeys.Exist == True, currentTable.columns]
+
+    ## Case 2: The schema has changed but the keys are the same
+    currentUpdated = outerJoin(currentTable, newTable)
+    changedSchema = outerJoin(currentUpdated, changedKeysRows)
+
+    ##  Case 1 or 2: Remove changed rows in current table
+    if not changedSchema.empty or not changedKeysRows.empty:
+
+        if not changedSchema.empty and changedKeysRows.empty:
+            print("not updatedRows.empty")
+            changedSchema.set_index('index', inplace=True, drop=True)
+            toDelete = changedSchema
+            deleteRows = syn.delete(Table(syn.get(tableSynId), toDelete))
+
+        if not changedKeysRows.empty and changedSchema.empty:
+            print("not changedKeysRows.empty")
+            changedKeysRows.set_index('index', inplace=True, drop=True)
+            toDelete = changedKeysRows
+            deleteRows = syn.delete(Table(syn.get(tableSynId), toDelete))
+
+        if not changedSchema.empty and not changedKeysRows.empty:
+            print("not updatedRows.empty and not changedKeysRows.empty")
+            changedSchema.set_index('index', inplace=True, drop=True)
+            changedKeysRows.set_index('index', inplace=True, drop=True)
+            toDelete = pandas.concat([changedKeysRows, changedSchema])
+            deleteRows = syn.delete(Table(syn.get(tableSynId), toDelete))
+
+        else:
+            print("No rows to delete in current table")
+
+    # Case 1,2,3: All new or updated rows to be added to table
+    newOrUpdated = outerJoin(newTable, currentTable)
+    newOrUpdated = newOrUpdated[schema]
+    if not newOrUpdated.empty:
+        print("Storing new and updated changes")
+        table = syn.store(Table(syn.get(tableSynId), newOrUpdated))
 
 
+def main():
+    for i, p in enumerate(paths):
+        data = json2flatten(p, names[i])
+        project_df = pandas.concat(data)
+        all_projects.append(project_df)
+
+    # append/concat/collapse all normalized dataframe annotations into one dataframe
+    all_projects_df = pandas.concat(all_projects)
+
+    # re-arrange columns/fields
+    all_projects_df = all_projects_df[
+        ["name", "description", "columnType", "maximumSize", "enumValues_value", "enumValues_description",
+         "enumValues_source", "project"]]
+
+    # html sources requires to be in utf-8 encode format
+    all_projects_df.enumValues_description = all_projects_df.enumValues_description.str.encode("utf-8")
+    all_projects_df.enumValues_source = all_projects_df.enumValues_source.str.encode("utf-8")
+    all_projects_df.enumValues_value = all_projects_df.enumValues_value.str.encode("utf-8")
+
+    all_projects_df.columns = ["key", "description", "columnType", "maximumSize", "value", "valuesDescription",
+                               "source", "project"]
+
+    # all_projects_df.to_csv("annot.csv", sep=',', index=False, encoding="utf-8")
+    # all_projects_df = pandas.read_csv('annot.csv', delimiter=',', encoding="utf-8")
+
+    updateTable(tableSynId=tableSynId, newTable=all_projects_df, key=key, delta=False, whereClause=False)
 
 
-
-
+if '__main__' == __name__:
+    main()
