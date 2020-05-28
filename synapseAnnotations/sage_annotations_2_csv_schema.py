1874bb81e16ee9a7757b41f9821cf83be06c5a6f
Kara Woo <kara.woo@sagebase.org>
2020-05-28 11:59:08 -0700

    Ensure that parents (keys) are added to CSV even if enumValues is empty

--- synapseAnnotations/sage_annotations_2_csv_schema.py
+++ synapseAnnotations/sage_annotations_2_csv_schema.py
@@ -113,10 +113,11 @@ for annotation_file in annotation_files:
                 # increment entity index
                 entity_idx += 1
 
-            # add parent/outer attribute to schema
-            csv_schema[entity_idx] = [attribute_name, description, valid_values, "", "", "TRUE", subclass_of, "", source]
-            print("- Added parent " + attribute_name + " for parent " + subclass_of)
-            entity_idx += 1
+        # add parent/outer attribute to schema
+        csv_schema[entity_idx] = [attribute_name, description, valid_values, "", "", "TRUE", subclass_of, "", source]
+
+        print("- Added parent " + attribute_name + " for parent " + subclass_of)
+        entity_idx += 1
 
 # construct a data frame containing this preliminary schema
 schema = pd.DataFrame.from_dict(csv_schema, orient = "index", columns = headers)
