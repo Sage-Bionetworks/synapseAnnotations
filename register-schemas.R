bf6be9b69535405a1ecabc7aad42ac1582fc2872
Kara Woo <karawoo@users.noreply.github.com>
2021-01-27 10:42:19 -0800

    Add workflow to check that schemas can be registered (#685)
    * Turn register-schemas.R into a little command line utility
    
    * Use renv for dependencies
    
    * Add workflow to register schemas (with dryRun option)
    
    * Remove out of date README content
    
    * Ensure that it's only files in terms/* that are tested on synapse
    
    * Exit gracefully if no files provided
    
    * Pipe files with xargs
    
    * Document how to register all schemas
    
    * Only attempt to register .json files in terms/
    
    * Remove unnecessary pwd call
    
    This was just for testing things out

--- register-schemas.R
+++ register-schemas.R
@@ -1,48 +1,123 @@
-############################################
-####  Register mini-schemas in Synapse  ####
-############################################
+#!/usr/local/bin/Rscript
+doc <- "Register Schemas on Synapse
 
-library("synapser")
-library("fs")
-library("here")
-library("purrr")
-library("glue")
+Usage:
+  register-schemas.R [--dryRun]
+  register-schemas.R <files>... [--dryRun]
+  register-schemas.R (-h | --help)
+  register-schemas.R --version
+
+Options:
+  -h --help    Show this screen.
+  --version    Show version.
+  --dryRun     Tests if the schema(s) can be registered, but does not actually
+               register them.
+"
+
+suppressPackageStartupMessages(library("docopt"))
+suppressPackageStartupMessages(library("synapser"))
+suppressPackageStartupMessages(library("purrr"))
+suppressPackageStartupMessages(library("glue"))
+
+opts <- docopt(doc, version = 'Register Schemas 0.1\n')
 
 synLogin()
 
-# Function to register JSON schema file ----------------------------------------
+# Functions to register JSON schema file ---------------------------------------
 
-create_body <- function(file) {
+#' Create schema request body
+#'
+#' @param file Path to JSON Schema file
+#' @param dryRun Should the dryRun field be set to `true`? Defaults to `FALSE`.
+#' @return String containing the JSON request body
+create_body <- function(file, dryRun = FALSE) {
+  stopifnot(dryRun %in% c(TRUE, FALSE))
   paste(
     '{"concreteType": "org.sagebionetworks.repo.model.schema.CreateSchemaRequest", "schema": ',
     paste(readLines(file), collapse = ""),
+    ', "dryRun": ',
+    ifelse(dryRun, 'true', 'false'),
     '}'
   )
 }
 
-register_schema <- function(file) {
+#' Register schema to Synapse
+#'
+#' Given a path to a file containing a JSON schema, registers that schema on
+#' Synapse.
+#'
+#' @param file Path to JSON Schema file
+#' @param dryRun Should the dryRun field be set to `true`? Defaults to `FALSE`.
+#' @return Token object containing the async token used to monitor the job
+register_schema <- function(file, dryRun = FALSE) {
   synRestPOST(
     uri = "/schema/type/create/async/start",
-    body = create_body(file)
+    body = create_body(file, dryRun = dryRun)
   )
+}
+
+#' Get status of registered schema
+#'
+#' @param token Async token
+#' @return Response object containing schema
+get_registration_status <- function(token) {
+  processing <- TRUE
+  ## Check results of registering schema, retrying if the async job hasn't
+  ## completed yet
+  while (processing) {
+    result <- synRestGET(
+      uri = glue("/schema/type/create/async/get/{token}")
+    )
+    ## synapser doesn't return the status codes unfortunately, so we check the
+    ## response object to determine what to do. If it contains "jobState",
+    ## that's part of the AsynchronousJobStatus, i.e. the async job hasn't
+    ## completed yet.
+    if (!"jobState" %in% names(result)) {
+      processing <- FALSE
+    }
+  }
+  result
+}
+
+#' Register schema and report on its status
+#'
+#' Registers a schema and then monitors the asynchronous job until it completes.
+#'
+#' @param file Path to JSON Schema file
+#' @param dryRun Should the dryRun field be set to `true`? Defaults to `FALSE`.
+#' @return `TRUE` if schema was successfully registered; `FALSE` otherwise.
+register_and_report <- function(file, dryRun = FALSE) {
+  message(glue("Registering {file} with dryRun = {dryRun}"))
+  ## register_schema() can fail if e.g. schemas contain non-ASCII characters --
+  ## in this case we catch that error and provide the message.
+  token <- try(register_schema(file, dryRun = dryRun), silent = TRUE)
+  if (inherits(token, "try-error")) {
+    message(token)
+    return(FALSE)
+  }
+  ## If we get a 400 status code, synapser will throw an error -- we want to
+  ## catch that and provide the message but not raise an actual R error.
+  status <- try(get_registration_status(token$token), silent = TRUE)
+  if (inherits(status, "try-error")) {
+    message(status)
+    FALSE
+  } else {
+    TRUE
+  }
 }
 
 # Register terms ---------------------------------------------------------------
 
-term_files <- dir_ls(here("terms"), regexp = "*\\.json", recurse = 1)
+if (length(opts$files) == 0) {
+  message("No files provided; nothing to register!")
+  quit(status = 0)
+}
 
 ## Register each mini-schema
-walk(term_files, register_schema)
+results <- map_lgl(opts$files, register_and_report, dryRun = opts$dryRun)
 
-## List all schemas to check they're there
-synRestPOST(
-  uri = "/schema/list",
-  body = '{organizationName: "sage.annotations"}'
-)
-
-# Register top-level schema ----------------------------------------------------
-
-token <- register_schema(here("schemas", "testschema.json"))
-
-# Retrieve schema
-synRestGET(uri = glue("/schema/type/create/async/get/{token$token}"))
+if (!all(results)) {
+  quit(status = 1)
+} else {
+  quit(status = 0)
+}
