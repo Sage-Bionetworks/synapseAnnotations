cd47a9aabbe74d37f7c3e59d6742bbae419a7206
Nicole Kauer <nmekauer@gmail.com>
2021-03-01 17:12:48 -0800

    Update table script to allow for terms aliased via reference

--- update-annotations-table.R
+++ update-annotations-table.R
@@ -20,7 +20,11 @@ library("here")
 #' @return The module or key name
 get_info <- function(id, info = c("module", "key")) {
   info <- match.arg(info)
-  pattern <- "^[^-]+-([[:alnum:]]+)\\.([[:alnum:]]+)-[0-9\\.]+"
+  if (grepl("-[0-9\\.]+", id)) {
+    pattern <- "^[^-]+-([[:alnum:]]+)\\.([[:alnum:]]+)-[0-9\\.]+"
+  } else {
+    pattern <- "^[^-]+-([[:alnum:]]+)\\.([[:alnum:]]+)"
+  }
   switch(
     info,
     module = gsub(pattern, "\\1", id),
@@ -28,12 +32,45 @@ get_info <- function(id, info = c("module", "key")) {
   )
 }
 
+#' Extract file path from referenced id
+#'
+#' @param id string containing the schema `$id`
+#' @return The local file path to the schema for the `$id`
+get_ref_path <- function(id) {
+  module <- get_info(id, "module")
+  key <- get_info(id, "key")
+  glue::glue("{here('terms')}/{module}/{key}.json")
+}
+
+#' Append enumerated values for a term
+#'
+#' @param data_table Table with columns key, description, columnType, module,
+#' maximumSize for adding the enumerated values of the key to
+#' @param json_list List of json data from the file
+#' @return Table with columns key, description, columnType, module, maximumSize,
+#' value, valueDescription, source, with one row per enumerated value
+add_enumerated_values <- function(data_table, json_list) {
+  enum_df <- json_list[["anyOf"]] %>%
+    rename(
+      value = const,
+      valueDescription = description
+    ) %>%
+      mutate(value = as.character(value))
+  return_df <- full_join(data_table, enum_df, by = character())
+  ## If no value had a source, then need to add this column
+  if (!("source" %in% names(dat))) {
+    return_df <- return_df %>%
+      add_column(source = NA)
+  }
+  return(return_df)
+}
+
 #' Create table row(s) for a term
 #'
 #' @param file Path to the JSON Schema file containing a term
 #' @return
 create_rows <- function(file) {
-  dat <- fromJSON(file)
+dat <- fromJSON(file)
   ## Information about the key
   return_df <- tibble::tibble(
     key = get_info(dat$`$id`, "key"),
@@ -45,23 +82,22 @@ create_rows <- function(file) {
   ## If term has enumerated values in anyOf, fromJSON will create a handy data
   ## frame of those. We use that in combination with return_df to create a row
   ## for each value.
+  ## If term references the enumerated values of another term, grab those
+  ## File referenced should exist in current set
   if ("anyOf" %in% names(dat)) {
-    enum_df <- dat[["anyOf"]] %>%
-      rename(
-        value = const,
-        valueDescription = description
-      ) %>%
-      mutate(value = as.character(value))
-    return_df <- full_join(return_df, enum_df, by = character())
-    ## If no value had a source, then need to add this column
-    if (!("source" %in% names(return_df))) {
-      return_df <- return_df %>%
-        add_column(source = NA)
-    }
+    return_df <- add_enumerated_values(data_table = return_df, json_list = dat)
+  } else if ("properties" %in% names(dat)) {
+    # Assume will be a reference to existing file with enumerated values
+    alias_dat <- fromJSON(get_ref_path(dat$properties[[return_df$key]]$`$ref`))
+    return_df <- add_enumerated_values(
+      data_table = return_df,
+      json_list = alias_dat
+    )
   } else {
     return_df <- return_df %>%
-      add_column(value = NA, valueDescription = NA, source = NA)
+    add_column(value = NA, valueDescription = NA, source = NA)
   }
+
   ## Select columns in order that matches the table schema
   select(
     return_df,
